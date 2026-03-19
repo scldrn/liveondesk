e0c40c066e7b7e7fbfb0f848dd413803cd049a24
sam <samuelcalderon.dev@gmail.com>
2026-03-18 20:10:13 -0500

    Initial Commit

---
+++ PetDesk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetDesk
+//
+//  Created by samuel on 18/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetDesk/PetDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetDeskApp.swift
+//  PetDesk
+//
+//  Created by samuel on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PetDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
